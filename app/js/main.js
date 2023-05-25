db7f7fe9dc85ac2543175f49bbbb5e572aecf73d
oilprint <irynakozlova.k@gmail.com>
2023-05-25 22:28:09 +0300

    added hero

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,13 @@
 $(function () {
-  var mixer = mixitup('.products__grid');
 
-  $('.review__inner').slick({
+  $('.hero__inner').slick({
     dots: true,
     arrows: true,
     fade: true,
-    prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><span class="sr-only">Предыдущий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="images/symbol-defs.svg#icon-chevron"></use></svg></button>',
-    nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><span class="sr-only">Следующий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="images/symbol-defs.svg#icon-chevron"></use></svg></button>'
+    autoplay: true,
+    autoplaySpeed: 2000,  
+    // prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><span class="sr-only">Предыдущий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="images/symbol-defs.svg#icon-chevron"></use></svg></button>',
+    // nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><span class="sr-only">Следующий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="images/symbol-defs.svg#icon-chevron"></use></svg></button>'
 
   });
   
--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,6 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'  
   ])
