b94e02500b9bf4ec5c6023144935f3f5de014820
oilprint <irynakozlova.k@gmail.com>
2023-05-31 17:50:09 +0300

    added hero__slider

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,10 @@ $(function () {
     dots: true,
     arrows: true,
     fade: true,
-    autoplay: true,
-    autoplaySpeed: 2000,  
-    // prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><span class="sr-only">Предыдущий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="images/symbol-defs.svg#icon-chevron"></use></svg></button>',
-    // nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><span class="sr-only">Следующий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="images/symbol-defs.svg#icon-chevron"></use></svg></button>'
-
+    // autoplay: true,
+    // autoplaySpeed: 2000,  
+    prevArrow: '<button class="slick-arrow slick-arrow--prev" type="button"><span class="sr-only">Предыдущий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="./images/symbol-defs.svg#icon-chevron"></use></svg></button>',
+    nextArrow: '<button class="slick-arrow slick-arrow--next" type="button"><span class="sr-only">Следующий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="./images/symbol-defs.svg#icon-chevron"></use></svg></button>'
   });
   
 });
