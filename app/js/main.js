021cc5573d4ad89c01806a9b5af93ae34d6da01b
oilprint <irynakozlova.k@gmail.com>
2023-06-01 10:18:24 +0300

    added review

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,29 @@ $(function () {
     dots: true,
     arrows: true,
     fade: true,
-    // autoplay: true,
-    // autoplaySpeed: 2000,  
+    autoplay: true,
+    autoplaySpeed: 2000,  
     prevArrow: '<button class="slick-arrow slick-arrow--prev" type="button"><span class="sr-only">Предыдущий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="./images/symbol-defs.svg#icon-chevron"></use></svg></button>',
     nextArrow: '<button class="slick-arrow slick-arrow--next" type="button"><span class="sr-only">Следующий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="./images/symbol-defs.svg#icon-chevron"></use></svg></button>'
   });
-  
+
+
+  $('.review__star').rateYo({
+    starWidth: "16px",
+    normalFill: "#989AA1",
+    ratedFill: "#F497AC",
+    readOnly: true,
+    spacing: "4px"
+  });
+
+  $('.review__inner').slick({
+   dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: '<button class="slick-arrow slick-arrow--prev" type="button"><span class="sr-only">Предыдущий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="./images/symbol-defs.svg#icon-chevron"></use></svg></button>',
+  nextArrow: '<button class="slick-arrow slick-arrow--next" type="button"><span class="sr-only">Следующий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="./images/symbol-defs.svg#icon-chevron"></use></svg></button>'
+});
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'  
   ])
