77295aa3d12ad3ee6d7a91669af727c182096df7
oilprint <irynakozlova.k@gmail.com>
2023-06-04 13:54:42 +0300

    add all links

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,7 @@ $(function () {
     arrows: true,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 1000,  
+    autoplaySpeed: 1500,  
     prevArrow: '<button class="slick-arrow slick-arrow--prev" type="button"><span class="sr-only">Предыдущий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="./images/symbol-defs.svg#icon-chevron"></use></svg></button>',
     nextArrow: '<button class="slick-arrow slick-arrow--next" type="button"><span class="sr-only">Следующий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="./images/symbol-defs.svg#icon-chevron"></use></svg></button>'
   });
